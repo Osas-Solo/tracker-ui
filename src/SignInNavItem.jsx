d713b7ca7b2fd7c46d20f12504a90e96fd81e3aa
Osaremhen Ukpebor <osas.solo@outlook.com>
2022-10-05 13:14:18 +0100

    First Commit

---
+++ src/SignInNavItem.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { NavItem, Modal, Button, NavDropdown, MenuItem } from "react-bootstrap";
+import withToast from "./withToast.jsx";
+
+class SigninNavItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showing: false,
+      user: { signedIn: false, givenName: "" },
+      disabled: true,
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+    this.signOut = this.signOut.bind(this);
+    this.signIn = this.signIn.bind(this);
+  }
+
+  componentDidMount() {
+    const clientId = window.ENV.GOOGLE_CLIENT_ID;
+    if (!clientId) return;
+    window.gapi.load("auth2", () => {
+      if (!window.gapi.auth2.getAuthInstance()) {
+        window.gapi.auth2.init({ client_id: clientId }).then(() => {
+          this.setState({ disabled: false });
+        });
+      }
+    });
+  }
+
+  async signIn() {
+    this.hideModal();
+    const { showError } = this.props;
+    let googleToken;
+
+    try {
+      const auth2 = window.gapi.auth2.getAuthInstance();
+      const googleUser = await auth2.signIn();
+      googleToken = googleUser.getAuthResponse().id_token;
+    } catch (error) {
+      showError(`Error authenticating with Google: ${error.error}`);
+    }
+
+    try {
+      const apiEndpoint = window.ENV.UI_AUTH_ENDPOINT;
+      const response = await fetch(`${apiEndpoint}/signin`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ google_token: googleToken }),
+      });
+      const body = await response.text();
+      const result = JSON.parse(body);
+      const { signedIn, givenName } = result;
+      this.setState({ user: { signedIn, givenName } });
+    } catch (error) {
+      showError(`Error signing into the app: ${error}`);
+    }
+  }
+
+  signOut() {
+    this.setState({ user: { signedIn: false, givenName: "" } });
+  }
+
+  showModal() {
+    const clientId = window.ENV.GOOGLE_CLIENT_ID;
+    const { showError } = this.props;
+    if (!clientId) {
+      showError("Missing environment variable GOOGLE_CLIENT_ID");
+      return;
+    }
+    this.setState({ showing: true });
+  }
+
+  hideModal() {
+    this.setState({ showing: false });
+  }
+
+  render() {
+    const { user } = this.state;
+    if (user.signedIn) {
+      return (
+        <NavDropdown title={user.givenName} id="user">
+          <MenuItem onClick={this.signOut}>Sign out</MenuItem>
+        </NavDropdown>
+      );
+    }
+
+    const { showing, disabled } = this.state;
+
+    return (
+      <>
+        <NavItem onClick={this.showModal}>Sign in</NavItem>
+        <Modal keyboard show={showing} onHide={this.hideModal} bsSize="sm">
+          <Modal.Header closeButton>
+            <Modal.Title>Sign in</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Button
+              block
+              disabled={disabled}
+              bsStyle="primary"
+              onClick={this.signIn}
+            >
+              <img
+                src="https://developers.google.com/identity/images/btn_google_signin_light_normal_web.png"
+                alt="Sign In"
+              />
+            </Button>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="link" onClick={this.hideModal}>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default withToast(SigninNavItem);
